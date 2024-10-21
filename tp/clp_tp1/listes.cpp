99a2856db7e9a82456ddac81556c2dc774238ed8
Lucas Tramonte <lucas.tramonte@student-cs.fr>
2024-10-21 23:07:19 +0200

    clp_tp1

--- tp/clp_tp1/listes.cpp
+++ tp/clp_tp1/listes.cpp
@@ -13,12 +13,304 @@
 #include <cstdlib>
 #include <ctime>
 
+//------------ 2.1. Création, affichage ------------
+
+using int_list_t = std::forward_list< int >;
+
+int_list_t random_list() {
+    int_list_t list;
+    int nb_elements = std::rand() % 10;  // Nombre d'éléments aléatoires entre 0 et 9
+
+    for( int i = 0; i < nb_elements; ++i ) {
+        int random_value = std::rand() % 100;  // Valeurs aléatoires entre 0 et 99
+        list.push_front( random_value );       
+    }
+    return list;
+}
+
+void print_list( const int_list_t &list ) {
+    std::cout << "( ";
+    for( auto elem : list ) {
+        std::cout << elem << " ";
+    }
+    std::cout << ")" << std::endl;
+}
+
+void test_21() {
+    std::cout << "*** test_21 ***" << std::endl;
+
+    int_list_t list = random_list();
+    print_list( list );
+}
+
+//------------ 2.2. Application ------------
+
+int_list_t map_iter(const int_list_t &list, std::function<int(int)> func) {
+    int_list_t result;
+    for (auto elem : list) {
+        result.push_front(func(elem));  
+    }
+    return result;  
+}
+
+void test_22() {
+    std::cout << "*** test_22 ***" << std::endl;
+
+    int_list_t list = random_list();
+
+    std::cout << "Liste initiale : ";
+    print_list(list);
+
+    int_list_t mapped_list = map_iter(list, [](int x) { return x * 3; });
+    std::cout << "Liste après map_iter (multiplication par 3) : ";
+    print_list(mapped_list);
+}
+
+//------------ 2.3. Filtrage ------------
+
+int_list_t filter_iter(const int_list_t &list, std::function<bool(int)> predicate) {
+    int_list_t result;
+    for (auto elem : list) {
+        if (predicate(elem)) {
+            result.push_front(elem);  
+        }
+    }
+    return result;  
+}
+
+void test_23() {
+    std::cout << "*** test_23 ***" << std::endl;
+
+    int_list_t list = random_list();
+
+    std::cout << "Liste initiale : ";
+    print_list(list);
+
+    int_list_t mapped_list = map_iter(list, [](int x) { return x * 3; });
+    std::cout << "Liste après map_iter (multiplication par 3) : ";
+    print_list(mapped_list);
+
+    int_list_t filtered_list = filter_iter(mapped_list, [](int x) { return x % 2 == 0; });
+
+    std::cout << "Liste après filter_iter (éléments pairs) : ";
+    print_list(filtered_list);
+}
+
+//------------ 2.4. Capture ------------
+
+void test_24() {
+    std::cout << "*** test_24 ***" << std::endl;
+
+    int_list_t list = random_list();
+    std::cout << "Liste initiale : ";
+    print_list(list);
+
+    int coef = std::rand() % 5 + 1;  // Coefficient aléatoire entre 1 et 5
+    std::cout << "Coefficient de multiplication : " << coef << std::endl;
+
+    int_list_t mapped_list = map_iter(list, [coef](int x) { return x * coef; });
+    std::cout << "Liste après map_iter (multiplication par " << coef << ") : ";
+    print_list(mapped_list);
+
+    int_list_t filtered_list = filter_iter(mapped_list, [](int x) { return x % 2 == 0; });
+    std::cout << "Liste après filter_iter (éléments pairs) : ";
+    print_list(filtered_list);
+}
+
+//------------ 2.5. Réduction ------------
+
+int reduce(const int_list_t &list, int initial_value, std::function<int(int, int)> func) {
+    int result = initial_value;
+    for (auto elem : list) {
+        result = func(result, elem);  
+    }
+    return result; 
+}
+
+void test_25() {
+    std::cout << "*** test_25 ***" << std::endl;
+
+    int_list_t list = random_list();
+
+    std::cout << "Liste initiale : ";
+    print_list(list);
+
+    int min_value = reduce(list, std::numeric_limits<int>::max(), [](int a, int b) {
+        return (a < b) ? a : b; 
+    });
+    std::cout << "Plus petit élément : " << min_value << std::endl;
+
+    int max_value = reduce(list, std::numeric_limits<int>::min(), [](int a, int b) {
+        return (a > b) ? a : b;  
+    });
+    std::cout << "Plus grand élément : " << max_value << std::endl;
+}
+
+//------------ 3.1. Récursion pour reduce() ------------
+
+int fold_left_aux(int_list_t::const_iterator it, int_list_t::const_iterator end, int initial_value, std::function<int(int, int)> func) {
+    if (it == end) {  
+        return initial_value;  
+    }
+    return fold_left_aux(std::next(it), end, func(initial_value, *it), func);
+}
+
+int fold_left(const int_list_t &list, int initial_value, std::function<int(int, int)> func) {
+    return fold_left_aux(list.cbegin(), list.cend(), initial_value, func);
+}
+
+void test_31() {
+    std::cout << "*** test_31 ***" << std::endl;
+
+    int_list_t list = random_list();
+    std::cout << "Liste initiale : ";
+    print_list(list);
+
+    int min_value = fold_left(list, std::numeric_limits<int>::max(), [](int a, int b) {
+        return (a < b) ? a : b;  
+    });
+    std::cout << "Plus petit élément : " << min_value << std::endl;
 
 
-int main()
-{
+    int max_value = fold_left(list, std::numeric_limits<int>::min(), [](int a, int b) {
+        return (a > b) ? a : b;  
+    });
+    std::cout << "Plus grand élément : " << max_value << std::endl;
+}
+
+//------------ 3.2. Récursion pour map() et filter() ------------
+
+void map_aux(int_list_t::const_iterator it, int_list_t::const_iterator end, std::function<int(int)> func, int_list_t &result) {
+    if (it == end) {  
+        return;
+    }
+    result.push_front(func(*it));  
+    map_aux(std::next(it), end, func, result);
+}
+
+int_list_t map(const int_list_t &list, std::function<int(int)> func) {
+    int_list_t result;  
+    map_aux(list.cbegin(), list.cend(), func, result);
+    result.reverse(); 
+    return result;
+}
+
+void filter_aux(int_list_t::const_iterator it, int_list_t::const_iterator end, std::function<bool(int)> pred, int_list_t &result) {
+    if (it == end) {  
+        return;
+    }
+    if (pred(*it)) {
+        result.push_front(*it);  
+    }
+    filter_aux(std::next(it), end, pred, result);
+}
+
+int_list_t filter(const int_list_t &list, std::function<bool(int)> pred) {
+    int_list_t result;  
+    filter_aux(list.cbegin(), list.cend(), pred, result);
+    result.reverse(); 
+    return result;
+}
+
+void test_32() {
+    std::cout << "*** test_32 ***" << std::endl;
+
+    int_list_t list = random_list();
+    std::cout << "Liste initiale : ";
+    print_list(list);
+
+    int coef = std::rand() % 5 + 1;  // Coefficient entre 1 et 5
+    std::cout << "Coefficient de multiplication : " << coef << std::endl;
+
+    int_list_t mapped_list = map(list, [coef](int x) { return x * coef; });
+    std::cout << "Liste après map() : ";
+    print_list(mapped_list);
+
+    int_list_t filtered_list = filter(mapped_list, [](int x) { return x % 2 == 0; });
+    std::cout << "Liste après filter() : ";
+    print_list(filtered_list);
+}
+
+//------------ 4.1. Application partielle ------------
+
+int_list_t map2(const int_list_t &list, std::function<int(int)> func) {
+    int_list_t result;  
+    for (auto elem : list) {
+        result.push_front(func(elem)); 
+    }
+    result.reverse();  
+    return result;
+}
+
+void test_41() {
+    std::cout << "*** test_41 ***" << std::endl;
+
+    int_list_t list = random_list();
+    std::cout << "Liste initiale : ";
+    print_list(list);
+
+    int coef = std::rand() % 5 + 1;  // Coefficient entre 1 et 5
+    std::cout << "Coefficient de multiplication : " << coef << std::endl;
+
+    std::multiplies<int> multiply_op;  
+    auto multiply_by_coef = [multiply_op, coef](int x) { return multiply_op(x, coef); };  
+
+    int_list_t mapped_list = map2(list, multiply_by_coef);
+    std::cout << "Liste après map() : ";
+    print_list(mapped_list);
+
+    int_list_t filtered_list = filter(mapped_list, [](int x) { return x % 2 == 0; });
+    std::cout << "Liste après filter() : ";
+    print_list(filtered_list);
+}
+
+//------------ 4.2. Réduction inversée ------------
+
+int fold_right(int_list_t::const_iterator it, int_list_t::const_iterator end, int init, std::function<int(int, int)> func) {
+    if (it == end) {
+        return init;  
+    } else {
+        return fold_right(std::next(it), end, func(*it, init), func);
+    }
+}
+
+void test_42() {
+    std::cout << "*** test_42 ***" << std::endl;
+
+    int_list_t list = random_list();
+    std::cout << "Liste initiale : ";
+    print_list(list);
+
+    auto add = [](int a, int b) { return a + b; };
+
+    int result_left = fold_left(list, 0, add);
+    int result_right = fold_right(list.cbegin(), list.cend(), 0, add);
+
+    std::cout << "Résultat de fold_left : " << result_left << std::endl;
+    std::cout << "Résultat de fold_right : " << result_right << std::endl;
+
+    if (result_left != result_right) {
+        std::cout << "Les résultats sont différents." << std::endl;
+    } else {
+        std::cout << "Les résultats sont identiques." << std::endl;
+    }
+}
+
+
+int main(){
     std::srand( std::time( nullptr ));
+    test_21();
+    test_22();
+    test_23();
+    test_24();
+    test_25();
+    test_31();
+    test_32();
+    test_41();
+    test_42();
 
     return 0;
 }
+
+
 
--- tp/clp_tp1/tris.cpp
+++ tp/clp_tp1/tris.cpp
@@ -13,11 +13,175 @@
 #include <cstdlib>
 #include <ctime>
 
+//------------ 1.1. Affichage d'un tableau ------------
+
+using int_tab_t = std::vector< int >;
+
+// Fonction pour afficher le tableau
+void print_tab( const int_tab_t &tab ) {
+    std::cout << "[ ";
+    for( const auto &elem : tab ) {
+        std::cout << elem << " ";
+    }
+    std::cout << "]" << std::endl;
+}
+
+// Fonction de test
+void test_11() {
+    std::cout << "*** test_11 ***" << std::endl;
+    
+    // Tableau de 6 entiers ayant les valeurs indiquées
+    const int_tab_t tab{ 1, -2, 3, -4, 5, -6 };
+    print_tab( tab );
+}
+
+//------------ 1.2. Remplissage d'un tableau avec des nombres aléatoires ------------
+
+// Fonction pour remplir le tableau avec des nombres aléatoires
+void random_tab( int_tab_t &tab ) {
+    for( auto &elem : tab ) {
+        // Générer un nombre aléatoire entre -10 et 10
+        elem = std::rand() % 21 - 10;
+    }
+}
+
+// Fonction de test pour la question 1.2
+void test_12() {
+    std::cout << "*** test_12 ***" << std::endl;
+
+    int_tab_t tab( 10 );
+    random_tab( tab );
+    print_tab( tab );
+}
+
+//------------ 1.3. Tri par ordre croissant ------------
+
+void sort_tab_1( int_tab_t &tab ) {
+    int n = tab.size();
+
+    for( int i = 0; i < n - 1; ++i ) {
+        int min_index = i;
+        // Trouver l'index du plus petit élément dans le reste du tableau
+        for( int j = i + 1; j < n; ++j ) {
+            if( tab[j] < tab[min_index] ) {
+                min_index = j;
+            }
+        }
+        std::swap( tab[i], tab[min_index] );
+    }
+}
+
+void test_13() {
+    std::cout << "*** test_13 ***" << std::endl;
+
+    int_tab_t tab( 10 );
+    random_tab( tab );
+
+    std::cout << "Tableau non trié : ";
+    print_tab( tab );
+    sort_tab_1( tab );
+
+    std::cout << "Tableau trié : ";
+    print_tab( tab );
+}
+
+//------------ 1.4. Tri selon un autre critère ------------
+
+// renvoie vrai si a < b
+bool less( int a, int b ) {
+    return a < b;
+}
+
+// renvoie vrai si a > b
+bool greater( int a, int b ) {
+    return a > b;
+}
+
+// prend un pointeur sur fonction de comparaison
+void sort_tab_2( int_tab_t &tab, bool (*compare)(int, int) ) {
+    int n = tab.size();
+
+    for( int i = 0; i < n - 1; ++i ) {
+        int min_or_max_index = i;
+        for( int j = i + 1; j < n; ++j ) {
+            if( compare(tab[j], tab[min_or_max_index]) ) {
+                min_or_max_index = j;
+            }
+        }
+        // Échanger l'élément trouvé avec l'élément à la position courante
+        std::swap( tab[i], tab[min_or_max_index] );
+    }
+}
+
+void test_14() {
+    std::cout << "*** test_14 ***" << std::endl;
+
+    int_tab_t tab( 10 );
+    random_tab( tab );
+
+    std::cout << "Tableau non trié : ";
+    print_tab( tab );
+
+    std::cout << "Tri avec less() (ordre croissant) : ";
+    sort_tab_2( tab, less );
+    print_tab( tab );
+
+    random_tab( tab );
+    std::cout << "Tableau non trié (après remplissage) : ";
+    print_tab( tab );
+
+    std::cout << "Tri avec greater() (ordre décroissant) : ";
+    sort_tab_2( tab, greater );
+    print_tab( tab );
+}
+
+//------------ 1.5. Tri avec fonction lambda ------------
+
+void sort_tab_3( int_tab_t &tab, std::function< bool(int, int) > compare ) {
+    int n = tab.size();
+
+    for( int i = 0; i < n - 1; ++i ) {
+        int min_or_max_index = i;
+        for( int j = i + 1; j < n; ++j ) {
+            if( compare(tab[j], tab[min_or_max_index]) ) {
+                min_or_max_index = j;
+            }
+        }
+        // Échanger l'élément trouvé avec l'élément à la position courante
+        std::swap( tab[i], tab[min_or_max_index] );
+    }
+}
+
+void test_15() {
+    std::cout << "*** test_15 ***" << std::endl;
+
+    int_tab_t tab( 10 );
+
+    random_tab( tab );
+    std::cout << "Tableau non trié : ";
+    print_tab( tab );
+
+    std::cout << "Tri croissant sur la valeur absolue : ";
+    sort_tab_3( tab, [](int a, int b) { return std::abs(a) < std::abs(b); });
+    print_tab( tab );
+
+    random_tab( tab );
+    std::cout << "Tableau non trié (après remplissage) : ";
+    print_tab( tab );
+
+    std::cout << "Tri décroissant sur la valeur absolue : ";
+    sort_tab_3( tab, [](int a, int b) { return std::abs(a) > std::abs(b); });
+    print_tab( tab );
+}
+
 
 int main()
 {
     std::srand( std::time( nullptr ));
-
+    test_11();
+    test_12();
+    test_13();
+    test_14();
+    test_15();
     return 0;
 }
-
